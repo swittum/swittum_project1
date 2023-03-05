4ab934275f59c58a19e78449f771a89edd48a7f1
swittum <122154159+swittum@users.noreply.github.com>
2023-03-05 16:16:20 +0100

    Styling the plot and adding a cos(x) line

--- newfile.py
+++ newfile.py
@@ -5,6 +5,10 @@ print("Hello World!")
 
 x = np.arange(0, 10, 0.01)
 y = np.sin(x)
+z = np.cos(x)
 
-plt.plot(x, y)
+plt.plot(x, y, label="sin(x)")
+plt.plot(x, z, label="cos(x)")
+plt.legend(loc="best")
+plt.grid(True)
 plt.show()
